3d72c6ff1284db7537cd8eb29cd908ea319c79cd
SHIV SANKAR <72968797+shiv1305@users.noreply.github.com>
2024-03-28 13:38:56 +0530

    Update Home.js

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -34,120 +34,121 @@ export default function Home() {
     <>
       <div>
         <Navbar />
-      </div>
-      <div>
-        <div
-          id="carouselExampleControls"
-          className="carousel slide"
-          data-bs-ride="carousel"
-          style={{ objectFit: "contain !important" }}
-        >
-          <div className="carousel-inner" id="carousal">
-            <div className="carousel-caption" style={{ zIndex: "10" }}>
-              <div className="d-flex justify-content-center">
-                <input
-                  className="form-control me-2"
-                  type="search"
-                  placeholder="Search"
-                  aria-label="Search"
-                  value={search}
-                  onChange={(e) => {
-                    setSearch(e.target.value);
-                  }}
+
+        <div>
+          <div
+            id="carouselExampleControls"
+            className="carousel slide"
+            data-bs-ride="carousel"
+            style={{ objectFit: "contain !important" }}
+          >
+            <div className="carousel-inner" id="carousal">
+              <div className="carousel-caption" style={{ zIndex: "10" }}>
+                <div className="d-flex justify-content-center">
+                  <input
+                    className="form-control me-2"
+                    type="search"
+                    placeholder="Search"
+                    aria-label="Search"
+                    value={search}
+                    onChange={(e) => {
+                      setSearch(e.target.value);
+                    }}
+                  />
+                </div>
+              </div>
+              <div className="carousel-item active">
+                <img
+                  src="https://source.unsplash.com/random/900x700/?pastrys"
+                  className="d-block w-100"
+                  style={{ filter: "brightness(30%)" }}
+                  alt="..."
+                />
+              </div>
+              <div className="carousel-item">
+                <img
+                  src="https://source.unsplash.com/random/900x700/?momos"
+                  className="d-block w-100"
+                  style={{ filter: "brightness(30%)" }}
+                  alt="..."
+                />
+              </div>
+              <div className="carousel-item">
+                <img
+                  src="https://source.unsplash.com/random/900x700/?pizzas"
+                  className="d-block w-100"
+                  style={{ filter: "brightness(30%)" }}
+                  alt="..."
                 />
               </div>
             </div>
-            <div className="carousel-item active">
-              <img
-                src="https://source.unsplash.com/random/900x700/?pastrys"
-                className="d-block w-100"
-                style={{ filter: "brightness(30%)" }}
-                alt="..."
-              />
-            </div>
-            <div className="carousel-item">
-              <img
-                src="https://source.unsplash.com/random/900x700/?momos"
-                className="d-block w-100"
-                style={{ filter: "brightness(30%)" }}
-                alt="..."
-              />
-            </div>
-            <div className="carousel-item">
-              <img
-                src="https://source.unsplash.com/random/900x700/?pizzas"
-                className="d-block w-100"
-                style={{ filter: "brightness(30%)" }}
-                alt="..."
-              />
-            </div>
+            <button
+              className="carousel-control-prev"
+              type="button"
+              data-bs-target="#carouselExampleControls"
+              data-bs-slide="prev"
+            >
+              <span
+                className="carousel-control-prev-icon"
+                aria-hidden="true"
+              ></span>
+              <span className="visually-hidden">Previous</span>
+            </button>
+            <button
+              className="carousel-control-next"
+              type="button"
+              data-bs-target="#carouselExampleControls"
+              data-bs-slide="next"
+            >
+              <span
+                className="carousel-control-next-icon"
+                aria-hidden="true"
+              ></span>
+              <span className="visually-hidden">Next</span>
+            </button>
           </div>
-          <button
-            className="carousel-control-prev"
-            type="button"
-            data-bs-target="#carouselExampleControls"
-            data-bs-slide="prev"
-          >
-            <span
-              className="carousel-control-prev-icon"
-              aria-hidden="true"
-            ></span>
-            <span className="visually-hidden">Previous</span>
-          </button>
-          <button
-            className="carousel-control-next"
-            type="button"
-            data-bs-target="#carouselExampleControls"
-            data-bs-slide="next"
-          >
-            <span
-              className="carousel-control-next-icon"
-              aria-hidden="true"
-            ></span>
-            <span className="visually-hidden">Next</span>
-          </button>
         </div>
-      </div>
-      <div className="container">
-        {foodCat !== [] ? (
-          foodCat.map((data) => {
-            return (
-              <div className="row m-3">
-                <div key={data._id} className="fs-3 m-3">
-                  {data.CategoryName}
+        <div className="container">
+          {foodCat !== [] ? (
+            foodCat.map((data) => {
+              return (
+                <div className="row m-3">
+                  <div key={data._id} className="fs-3 m-3">
+                    {data.CategoryName}
+                  </div>
+                  <hr></hr>
+                  {foodItem !== [] ? (
+                    foodItem
+                      .filter(
+                        (item) =>
+                          item.CategoryName === data.CategoryName &&
+                          item.name
+                            .toLowerCase()
+                            .includes(search.toLocaleLowerCase())
+                      )
+                      .map((filterItems) => {
+                        return (
+                          <div
+                            key={filterItems._id}
+                            className="col-12 col-md-6 col-lg-3"
+                          >
+                            <Card
+                              foodItem={filterItems}
+                              options={filterItems.options[0]}
+                            ></Card>
+                          </div>
+                        );
+                      })
+                  ) : (
+                    <div>No such data Found</div>
+                  )}
                 </div>
-                <hr></hr>
-                {foodItem !== [] ? (
-                  foodItem
-                    .filter(
-                      (item) =>
-                        item.CategoryName === data.CategoryName &&
-                        item.name
-                          .toLowerCase()
-                          .includes(search.toLocaleLowerCase())
-                    )
-                    .map((filterItems) => {
-                      return (
-                        <div
-                          key={filterItems._id}
-                          className="col-12 col-md-6 col-lg-3"
-                        >
-                          <Card
-                            foodItem={filterItems}
-                            options={filterItems.options[0]}
-                          ></Card>
-                        </div>
-                      );
-                    })
-                ) : (
-                  <div>No such data Found</div>
-                )}
-              </div>
-            );
-          })
-        ) : (
-          <div>"""""""</div>
-        )}
+              );
+            })
+          ) : (
+            <div>"""""""</div>
+          )}
+        </div>
       </div>
 
       <div>
